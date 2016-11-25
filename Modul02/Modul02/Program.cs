e0f0620f3b9b80d7427b803f2d4a49d018539b2e
mstaycheva <mstaycheva@hotmail.com>
2016-11-25 15:35:41 +0200

    Модул 02. Жидео 01 - добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дефиниране на променливи
+
+
+            //Събиране на числа
+
+
+            //Писане в конзолата
+        }
+    }
+}
